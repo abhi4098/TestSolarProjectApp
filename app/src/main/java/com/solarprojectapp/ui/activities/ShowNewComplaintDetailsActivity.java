de23b8b7cef5a6a1cb7e688da2c93bb782127bc9
Abhinandan <abhi08401@gmail.com>
2018-03-03 21:00:42 +0530

    new complaint

---
+++ app/src/main/java/com/solarprojectapp/ui/activities/ShowNewComplaintDetailsActivity.java
@@ -0,0 +1,109 @@
+package com.solarprojectapp.ui.activities;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.solarprojectapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class ShowNewComplaintDetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Context mContext;
+
+    @BindView(R.id.back_icon)
+    ImageView ivBackIcon;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tv_app_title)
+    TextView tvAppTitle;
+
+    @BindView(R.id.end_consumer)
+    TextView tvEndConsumer;
+    @BindView(R.id.project_owner)
+    TextView tvProjectOwner;
+    @BindView(R.id.state)
+    TextView tvprojectState;
+    @BindView(R.id.complaint_id)
+    TextView tvComplaintId;
+    @BindView(R.id.complaint_name)
+    TextView tvComplaintName;
+    @BindView(R.id.description)
+    TextView tvDescription;
+    @BindView(R.id.project_type)
+    TextView tvProjectType;
+    @BindView(R.id.contact_no)
+    TextView tvContactNum;
+
+
+
+
+    //private EditText result;
+   String  complaintDesc,complaintId,complaintName,complaintEndConsumer,complaintProjectOwner,complaintProjectType,complaintState,complaintContact;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_show_new_complaint_details);
+        mContext = ShowNewComplaintDetailsActivity.this;
+        ButterKnife.bind(this);
+        ivBackIcon.setOnClickListener(this);
+        tvAppTitle.setText("NEW COMPLAINT DETAILS");
+        complaintDesc = getIntent().getExtras().getString("COMPLAINT_DESC");
+        complaintId = getIntent().getExtras().getString("COMPLAINT_ID");
+        complaintName = getIntent().getExtras().getString("COMPLAINT");
+        complaintEndConsumer = getIntent().getExtras().getString("COMPLAINT_END_CONSUMER");
+        complaintProjectOwner = getIntent().getExtras().getString("COMPLAINT_PROJECT_OWNER");
+        complaintProjectType = getIntent().getExtras().getString("COMPLAINT_PROJECT_TYPE");
+        complaintState = getIntent().getExtras().getString("COMPLAINT_STATE");
+        complaintContact = getIntent().getExtras().getString("COMPLAINT_CONTACT");
+
+        tvComplaintId.setText(complaintId);
+        tvComplaintName.setText(complaintName);
+        tvProjectType.setText(complaintProjectType);
+        tvContactNum.setText(complaintContact);
+        tvprojectState.setText(complaintState);
+        tvDescription.setText(complaintDesc);
+        tvProjectOwner.setText(complaintProjectOwner);
+        tvEndConsumer.setText(complaintEndConsumer);
+
+
+
+
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+
+        super.onBackPressed();
+
+    }
+}
+
+
--- app/src/main/java/com/solarprojectapp/ui/adapters/NewComplaintAdapter.java
+++ app/src/main/java/com/solarprojectapp/ui/adapters/NewComplaintAdapter.java
@@ -1,18 +1,26 @@
 package com.solarprojectapp.ui.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.solarprojectapp.R;
 import com.solarprojectapp.generated.model.ComplaintListsDatum;
+import com.solarprojectapp.ui.activities.ShowNewComplaintDetailsActivity;
+import com.solarprojectapp.ui.activities.SparePartsRequestedActivity;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Abhinandan on 26/12/17.
@@ -41,7 +49,7 @@ public class NewComplaintAdapter extends ArrayAdapter<ComplaintListsDatum> {
         public TextView projectOwner;
         public TextView endConsumer;
         public TextView projectType;
-
+        public Button viewDetailsBtn;
 
 
 
@@ -65,6 +73,8 @@ public class NewComplaintAdapter extends ArrayAdapter<ComplaintListsDatum> {
             viewHolder.endConsumer= (TextView) rowView.findViewById(R.id.end_consumer);
             viewHolder.projectOwner= (TextView) rowView.findViewById(R.id.project_owner);
             viewHolder.projectType= (TextView) rowView.findViewById(R.id.project_type);
+            viewHolder.viewDetailsBtn= (Button) rowView.findViewById(R.id.view_detail_button);
+
 
 
 
@@ -82,6 +92,26 @@ public class NewComplaintAdapter extends ArrayAdapter<ComplaintListsDatum> {
             holder.endConsumer.setText(complaintListsDatum.getEndConsumer());
             holder.projectType.setText(complaintListsDatum.getProjectType());
             holder.projectOwner.setText(complaintListsDatum.getProjectOwner());
+            holder.viewDetailsBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getContext(), ShowNewComplaintDetailsActivity.class);
+                    i.putExtra("COMPLAINT_DESC",complaintListsDatum.getComplainDescription());
+                    i.putExtra("COMPLAINT_ID",complaintListsDatum.getComplainId());
+                    i.putExtra("COMPLAINT",complaintListsDatum.getComplaint());
+                    i.putExtra("COMPLAINT_END_CONSUMER",complaintListsDatum.getEndConsumer());
+                    i.putExtra("COMPLAINT_PROJECT_OWNER",complaintListsDatum.getProjectOwner());
+                    i.putExtra("COMPLAINT_PROJECT_TYPE",complaintListsDatum.getProjectType());
+                    i.putExtra("COMPLAINT_STATE",complaintListsDatum.getState());
+                    i.putExtra("COMPLAINT_CONTACT",complaintListsDatum.getEndConsumerContactno());
+                    //i.putExtra("INTENT_FROM","EditButton");
+
+                    getContext().startActivity(i);
+
+                }
+            });
+
+
 
         }
 
